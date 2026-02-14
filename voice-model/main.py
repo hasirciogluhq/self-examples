ebeaff69378a2690151ea17dc501445f35ae5566
hasirciogluhq <mhasirciogli@gmail.com>
2026-02-14 15:03:05 +0300

    Refactor code structure for improved readability and maintainability

--- voice-model/main.py
+++ voice-model/main.py
@@ -6,8 +6,11 @@ pipeline = KPipeline(lang_code='a')
 text = '''
 Ladies end gentlemen's! Welcome back to our new reality show!
 '''
-generator = pipeline(text, voice='af_heart')
-for i, (gs, ps, audio) in enumerate(generator):
+generator1 = pipeline(text, voice='af_heart')
+generator2 = pipeline(text, voice='af_heart')
+generator3 = pipeline(text, voice='af_heart')
+generator4 = pipeline(text, voice='af_heart')
+for i, (gs, ps, audio) in enumerate(generator1):
     print(i, gs, ps)
     display(Audio(data=audio, rate=24000, autoplay=i==0))
-    sf.write(f'{i}.wav', audio, 24000)
+    sf.write(f'1{i}.wav', audio, 30000)
