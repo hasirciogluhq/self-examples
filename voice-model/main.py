adc6706949bed0a835c834c629166198c5adb46b
hasirciogluhq <mhasirciogli@gmail.com>
2026-02-14 14:59:43 +0300

    Refactor code structure for improved readability and maintainability

--- voice-model/main.py
+++ voice-model/main.py
@@ -4,7 +4,7 @@ import soundfile as sf
 import torch
 pipeline = KPipeline(lang_code='a')
 text = '''
-[Kokoro](/kˈOkəɹO/) is an open-weight TTS model with 82 million parameters. Despite its lightweight architecture, it delivers comparable quality to larger models while being significantly faster and more cost-efficient. With Apache-licensed weights, [Kokoro](/kˈOkəɹO/) can be deployed anywhere from production environments to personal projects.
+Ladies end gentlemen's! Welcome back to our new reality show!
 '''
 generator = pipeline(text, voice='af_heart')
 for i, (gs, ps, audio) in enumerate(generator):
